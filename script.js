7f63f725e3666d03d5c0f9ddd8b2212a668db606
MODS <67655889+Gamermitobr@users.noreply.github.com>
2025-12-08 10:28:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const apiKey = 'AIzaSyD76062dyWycPGBt2zWqOV17kHdx4q9ARM';
-const videoId = 'VIDEO_ID';
-const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
+const canalId = 'https://youtube.com/@jpfilmes-x4h?si=MG_8AOotO4gJ_TBO'; // ou 'https://www.youtube.com/c/NOME_DO_CANAL'
+const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${canalId}&key=${apiKey}`;
 
 fetch(url)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+    const videos = data.items;
+    videos.forEach(video => {
+      console.log(video.snippet.title);
+      console.log(video.snippet.thumbnails.default.url);
+      console.log(video.snippet.description);
+    });
+  })
   .catch(error => console.error(error));
