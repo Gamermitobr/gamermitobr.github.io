32bf10c92f2d2365d4886f71a6821eab073e39c2
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-04-16 16:30:08 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
 import { supabase } from './supabase.js'
 
+async function checkUser() {
+  const { data } = await supabase.auth.getUser()
+  const user = data.user
+
+  const loginLink = document.getElementById('login-link')
+  const userInfo = document.getElementById('user-info')
+  const logoutBtn = document.getElementById('logout')
+
+  if (user) {
+    loginLink.style.display = "none"
+    logoutBtn.style.display = "inline"
+    userInfo.innerText = "Logado: " + user.email
+  } else {
+    loginLink.style.display = "inline"
+    logoutBtn.style.display = "none"
+    userInfo.innerText = ""
+  }
+
+  if (logoutBtn) {
+    logoutBtn.onclick = async () => {
+      await supabase.auth.signOut()
+      location.reload()
+    }
+  }
+}
+
 async function loadCampaigns() {
   const container = document.getElementById('campaigns')
 
@@ -34,4 +60,5 @@ async function loadCampaigns() {
   `).join('')
 }
 
+checkUser()
 loadCampaigns()
