2381574cf682ff79f121e706b4e94a7d22545c6e
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-04-16 16:09:50 -0300

    Atualizar o supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co"
-const SUPABASE_KEY = "SUA-ANON-KEY"
+const SUPABASE_URL = "https://niedwjyzvmkgwfdwhacf.supabase.co"
+const SUPABASE_KEY = "sb_publishable_izTWyBI32u5erxhM0vzxeQ_4O7v92Yh"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
